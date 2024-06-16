ef35f2a7792fa42532d7e343bf49a63f22f0b279
Gustavo Ferreira <65983794+gustavofdeoliveira@users.noreply.github.com>
2024-06-16 17:36:09 -0300

    feat: backend

--- ponderada-4/microsservices/photo/app/services/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.services.auth_service import AuthService
-from app.services.photo_service import PhotoService
-from app.services.user_service import UserService
---
+++ ponderada-4/user/app/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+
+from app.api.v1.endpoints.auth import router as auth_router
+from app.api.v1.endpoints.user import router as user_router
+
+from app.core.config import configs
+from app.core.container import Container
+from app.util.class_object import singleton
+import logging
+
+# Configuração básica do logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Adicionar handler para exibir logs no console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+@singleton
+class AppCreator:
+    def __init__(self):
+        # set app default
+        self.app = FastAPI(
+            title=configs.PROJECT_NAME,
+            openapi_url=f"{configs.API}/openapi.json",
+            version="0.0.1",
+            contact={
+                "name": "Gustavo F. de Oliveira",
+                "url": "https://github.com/gustavofdeoliveira/",
+                "email": "gustavo.oliveira@sou.inteli.edu.br",
+            },
+            license_info={
+                "name": "Apache 2.0",
+                "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+            },
+        )
+
+        # set db and container
+        self.container = Container()
+        self.db = self.container.db()
+        # self.db.create_database()
+
+        # set cors
+        if configs.BACKEND_CORS_ORIGINS:
+            self.app.add_middleware(
+                CORSMiddleware,
+                allow_origins=[str(origin) for origin in configs.BACKEND_CORS_ORIGINS],
+                allow_credentials=True,
+                allow_methods=["*"],
+                allow_headers=["*"],
+            )
+
+        # set routes
+        @self.app.get("/")
+        def root():
+            return "service is working"
+        
+        self.app.include_router(auth_router)
+        self.app.include_router(user_router)
+
+
+app_creator = AppCreator()
+app = app_creator.app
+db = app_creator.db
+container = app_creator.container
+
+@app.on_event("startup")
+async def startup_event():
+    container.init_resources()
+    container.wire(modules=[__name__])
+    logger.info("Application startup")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Application shutdown")
